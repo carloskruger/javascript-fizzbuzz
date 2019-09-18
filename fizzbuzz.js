8c0bb6565553a867546b2e0468264c2885498b6b
Carlos Kruger <carloskr@yahoo.com>
2019-09-18 16:57:12 -0400

    fizzbuzz homework

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 101; i++ ){
+    if (i % 3 === 0){
+
+        if (i % 5 === 0) console.log(i, "fizzbuzz")
+        else console.log(i, "fizz")
+    } 
+    else {if (i % 5 === 0) console.log(i, "buzz") 
+    else 
+    console.log(i)}
+
+}
